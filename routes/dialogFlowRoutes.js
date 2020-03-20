0c5d6d9dff706084b4d29e8304dda95aa5e42908
Marco <marco_ambrosio@hotmail.com>
2020-03-19 20:16:37 -0500

    connect server side and dialogflow

---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,20 @@
+'use strict';
+const chatbot = require ('../chatbot/chatbot');
+
+module.exports = app =>{
+    app.get('/',(req, res)=>{
+        res.send({'hello':'Marco query y event..sessionClient ..Path/Detected intent- credential/async/await/post 2'})
+     });
+    
+     app.post('/api/df_text_query', async(req, res)=>{
+     let responses= await chatbot.textQuery(req.body.text, req.body.parameters);
+     res.send(responses[0].queryResult);
+     console.log(req.body);
+     });   
+
+     app.post('/api/df_event_query', async(req, res)=>{
+      let responses= await chatbot.eventQuery(req.body.event, req.body.parameters);
+      res.send(responses[0].queryResult);
+      console.log(req.body);
+      });    
+}
