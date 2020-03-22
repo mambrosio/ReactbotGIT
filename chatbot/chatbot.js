500b52225e1f4cc4eef32ca804e6c5d440e2a6bf
Marco <marco_ambrosio@hotmail.com>
2020-03-22 15:44:32 -0500

    prueba commit

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,60 +1,61 @@
 'use strict';
-const dialogflow = require('dialogflow');
-const structjson = require('./structjson.js');
+
+const dialogFlow = require('dialogFlow');
 const config = require('../config/keys');
-const projectId = config.googleProjectID;
-const sessionId = config.dialogFlowSessionID;
-const languageCode = config.dialogFlowSessionLanguageCode;
 
-const credentials = {
-    client_email: config.googleClientEmail,
-    private_key : config.googlePrivateKey,
-};
+const sessionClient = new dialogFlow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
-const sessionClient = new dialogflow.SessionsClient({projectId, credentials});
-module.exports = {
-    textQuery: async function(text, userID, parameters = {}) {
-        let self = module.exports;
-        const sessionPath = sessionClient.sessionPath(projectId, sessionId + userID);
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    text: text,
-                    languageCode: languageCode,
-                },
-            },
-            queryParams: {
-                payload: {
-                    data: parameters
-                }
-            }
-        };
-        let responses = await sessionClient.detectIntent(request);
-        responses = await self.handleAction(responses);
-        return responses;
-    },
-    eventQuery: async function(event, userID,  parameters = {}) {
-        let self = module.exports;
-        let sessionPath = sessionClient.sessionPath(projectId, sessionId + userID);
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                event: {
-                    name: event,
-                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
-                    languageCode: languageCode,
-                },
-            }
-        };
+module.exports={
+textQuery: async function(text, parameters= {}) {
+    let self = module.exports;
+    const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: text,
+            languageCode: config.dialogFlowSessionLanguajeCode
+          },
+        },
+        queryParams: {
+        payload: {
+        data: parameters
+              }
+        }
+    };
+    let responses = await sessionClient.detectIntent(request);
+      responses = await self.handleAction(responses);
+      return responses;
+   },
+//// event
+eventQuery: async function(event, parameters= {}) {
+    
+    let self = module.exports;
+    const request = {
+        session: sessionPath,
+        queryInput: {
+          event: {
+            name: event,
+            languageCode: config.dialogFlowSessionLanguajeCode
+          },
+        },
+        queryParams: {
+        payload: {
+        data: parameters
+              }
+        }
+    };
+    let responses = await sessionClient.detectIntent(request);
+      responses = await self.handleAction(responses);
+      return responses;
+   },
 
-        let responses = await sessionClient.detectIntent(request);
-        responses = await self.handleAction(responses);
-        responses = self.handleAction(responses);
-        return responses;
 
-    },
-    handleAction: function(responses){
-        return responses;
-    },
+
+
+
+/////
+   handleAction: function (responses){
+     return responses;
+   }
 }
