9420835dbf7433a46049d726464e8df53da93ad7
Marco <marco_ambrosio@hotmail.com>
2020-03-29 10:56:07 -0500

    first commit 055

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,6 @@ module.exports ={
     dialogflowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogflowSessionLanguajeCode: process.env.DIALOGFLOW_LANGUGAGE_CODE,
     googleClientEmail:   process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey:    JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+    google_Private_Key:    JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
 
     }
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
+const serveStatic = require('serve-static')
 const app=express();
 
 app.use(bodyParser.json());
 require('./routes/dialogflowRoutes')(app);
 
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
 const newLocal = 5000;
 const PORT =process.env.PORT || newLocal;
 app.listen(PORT);
+console.log('server started ' + port)
 
